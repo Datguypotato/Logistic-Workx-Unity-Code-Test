9d39a15435cf93e1b33aceae225a684dec6e36c0
Gordon home PC <aardappel156@gmail.com>
2022-02-18 23:13:53 +0100

    filled ifs and show what the player is currently holding

--- Assets/Script/InventoryMaster.cs
+++ Assets/Script/InventoryMaster.cs
@@ -1,7 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryMaster : MonoBehaviour
 {
@@ -11,11 +9,23 @@ public class InventoryMaster : MonoBehaviour
     [SerializeField]
     private int ySize;
 
+    [SerializeField]
+    private GameObject canvasObject;
+
     //
     //private bool activeInventory;
 
     // the current item you are holding
+    [SerializeField] // debug reasons
     private Item currentItem;
+
+    // mouse properties
+    [SerializeField]
+    private Transform mouseFollowerPrefab;
+
+    private RectTransform mouseFollower;
+    private Image mouseImage;
+    private Vector2 pos;
 
     [SerializeField]
     private GameObject slotPrefab;
@@ -23,10 +33,16 @@ public class InventoryMaster : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
+        // setup grid cellSize
         GridLayoutGroup grid = GetComponent<GridLayoutGroup>();
         float cellSize = grid.padding.left + grid.padding.right + grid.cellSize.x;
 
         GetComponent<RectTransform>().sizeDelta = new Vector2(cellSize * xSize, cellSize * ySize);
+
+        // setup mouse follower
+        mouseFollower = Instantiate(mouseFollowerPrefab, canvasObject.transform).GetComponent<RectTransform>();
+        mouseFollower.SetParent(canvasObject.transform);
+        mouseImage = mouseFollower.GetComponent<Image>();
 
         for (int x = 0; x < xSize; x++)
         {
@@ -36,6 +52,22 @@ public class InventoryMaster : MonoBehaviour
                 slotObject.GetComponent<InventorySlot>().Assign(this);
             }
         }
+    }
+
+    private void Update()
+    {
+        // TODO Show current item holding
+
+        // show/hide mousefollow sprite
+        mouseImage.color = currentItem == null ? Color.clear : Color.white;
+
+        if (currentItem == null)
+            return;
+
+        // sprite following mouse when holding a item
+
+        // https://stackoverflow.com/questions/43802207/position-ui-to-mouse-position-make-tooltip-panel-follow-cursor
+        mouseFollower.anchoredPosition = Input.mousePosition / canvasObject.transform.localScale.x;
     }
 
     public Item GetCurrentItem()
--- Assets/Script/Item.cs
+++ Assets/Script/Item.cs
@@ -2,6 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour
+[CreateAssetMenu(fileName = "New item", menuName = "Item", order = 1)]
+public class Item : ScriptableObject
 {
+    [SerializeField]
+    public string itemName;
+
+    public Sprite itemSprite;
+
+    public int maxStack;
+    public int currentAmount = 1;
 }
